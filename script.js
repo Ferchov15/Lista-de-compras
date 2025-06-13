95af47db016ae1cd427d2a9c0cc51ddd3f1d829f
Fernando Vaca <fdvaca@puce.edu.ec>
2025-06-13 00:36:56 -0500

    Actualización 1

--- script.js
+++ script.js
@@ -2,36 +2,32 @@ let boton = document.getElementById("botonanadir");
 let input = document.getElementById("input");
 let list = document.getElementById("lista");
 
+boton.addEventListener("click", () => {
+  let myItem = input.value;
+  if (myItem !== "") {
+    input.value = "";
+    let listItem = document.createElement("li");
+    let listText = document.createElement("span");
+    let listBtncomprar = document.createElement("button");
+    let listBtneliminar = document.createElement("button");
 
-boton.addEventListener("click",()=>{
-    let myItem = input.value;
-    input.value = '';
-    let listItem = document.createElement('li');
-    let listText = document.createElement('span');
-    let listBtncomprar = document.createElement('button');
-    let listBtneliminar = document.createElement('button');
-
-    
     listItem.appendChild(listText);
     listText.textContent = myItem;
     listItem.appendChild(listBtncomprar);
-    listBtncomprar.textContent = 'Seleccionar';
+    listBtncomprar.textContent = "Seleccionar";
     listItem.appendChild(listBtneliminar);
-    listBtneliminar.textContent = 'Eliminar';
+    listBtneliminar.textContent = "Eliminar";
 
-    
-    list.appendChild(listItem);  
+    list.appendChild(listItem);
 
     listBtncomprar.onclick = (e) => {
-        listText.style.textDecoration = 'line-through';
-    }
-    
+      listText.style.textDecoration = "line-through";
+    };
+
     listBtneliminar.onclick = (e) => {
-        list.removeChild(listItem);
-    }
+      list.removeChild(listItem);
+    };
+  }
 
-input.focus();
-})
-
-
-
+  input.focus();
+});
